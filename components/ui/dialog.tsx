932545f017b1d63e76f107faaa8f7559d2ae1fb1
cristhianjhl.com <cristhianjhl.web@gmail.com>
2026-03-12 04:01:51 -0500

    style(ui): align buttons and dialogs with rounded-sm

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -33,7 +33,7 @@ function DialogContent({
       <DialogOverlay />
       <DialogPrimitive.Content
         className={cn(
-          "fixed left-[50%] top-[50%] z-50 w-[min(92vw,520px)] translate-x-[-50%] translate-y-[-50%] rounded-2xl border border-border bg-background p-6 shadow-2xl outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
+          "fixed left-[50%] top-[50%] z-50 w-[min(92vw,520px)] translate-x-[-50%] translate-y-[-50%] rounded-sm border border-border bg-background p-6 shadow-none outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
           className
         )}
         {...props}
